7641d9a0ce3f0c6c34651e593c74b1d1b9410c27
Jerry Liu <jerrylzy@ucla.edu>
2022-03-10 15:48:48 -0800

    Add dropout to the output layer

--- layers.py
+++ layers.py
@@ -306,6 +306,7 @@ class BiDAFOutput(nn.Module):
         super(BiDAFOutput, self).__init__()
         self.att_linear_1 = nn.Linear(10 * hidden_size, 1)
         self.mod_linear_1 = nn.Linear(4 * hidden_size, 1)
+        self.dropout_1 = nn.Dropout(drop_prob)
 
         self.rnn = RNNEncoder(input_size=4 * hidden_size,
                               hidden_size=2 * hidden_size,
@@ -314,12 +315,13 @@ class BiDAFOutput(nn.Module):
 
         self.att_linear_2 = nn.Linear(10 * hidden_size, 1)
         self.mod_linear_2 = nn.Linear(4 * hidden_size, 1)
+        self.dropout_2 = nn.Dropout(drop_prob)
 
     def forward(self, att, mod, mask):
         # Shapes: (batch_size, seq_len, 1)
-        logits_1 = self.att_linear_1(att) + self.mod_linear_1(mod)
+        logits_1 = self.dropout_1(self.att_linear_1(att) + self.mod_linear_1(mod))
         mod_2 = self.rnn(mod, mask.sum(-1))
-        logits_2 = self.att_linear_2(att) + self.mod_linear_2(mod_2)
+        logits_2 = self.dropout_2(self.att_linear_2(att) + self.mod_linear_2(mod_2))
 
         # Shapes: (batch_size, seq_len)
         log_p1 = masked_softmax(logits_1.squeeze(), mask, log_softmax=True)
