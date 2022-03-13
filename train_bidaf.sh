76d65713c61c87143fb429c3c0bd48c4a0e320f9
Jerry Liu <jerrylzy@ucla.edu>
2022-03-13 01:53:56 -0800

    Update training arguments

--- train_bidaf.sh
+++ train_bidaf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python train.py -n qanet-cnn0311202210/bidaf/nocnn/no-kaiming-init/hs128 --eval_after_epoch true --hidden_size 128 --seed 0 --amp true #--use_char_cnn true --l2_wd 0.0000003
+python train.py -n ensemble/bidaf/nocnn/hs100 --eval_after_epoch true --hidden_size 100 --seed 0 --amp true #--use_char_cnn true #--l2_wd 0.0000003
--- train_qanet.sh
+++ train_qanet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python train.py -n qanet-cnn-0311202210/no-cnn/qanet-base --lr 0.001 --drop_prob 0.1 --qanet true --project true --hidden_size 128 --batch_size 32 --l2_wd 0.0000003 --eval_after_epoch true --seed 0 --amp true #--use_char_cnn true
+python train.py -n ensemble/cnn/qanet-base-stochastic-depth --lr 0.001 --drop_prob 0.1 --qanet true --project true --hidden_size 128 --batch_size 16 --l2_wd 0.0000003 --eval_after_epoch true --seed 0 --amp true --use_char_cnn true
